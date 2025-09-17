c150fe0399651c3aa919ec08e7f5adce2b4fda45
Colin Cross <ccross@android.com>
2025-09-17 13:49:57 -0700

    Build blueprint_tools and run blueprint_tests in Soong-only builds
    Make builds set blueprint_tools as a dependency of droid_targets and
    blueprint_tests as a dependency of checkbuild, add the same dependencies
    to Soong-only builds.
    
    Test: aninja -t path checkbuild out/host/linux-x86/bin/go/gob_gen/linux_glibc_x86_64/test/gob_gen.a
    Change-Id: Id50efdde88bae7dde87ee76348a704cd39365dca

--- android/module.go
+++ android/module.go
@@ -3438,6 +3438,10 @@ func (c *buildTargetSingleton) GenerateBuildActions(ctx SingletonContext) {
 		}
 	})
 
+	if !ctx.Config().UnbundledBuildImage() {
+		checkbuildDeps = append(checkbuildDeps, PathForPhony(ctx, "blueprint_tests"))
+	}
+
 	suffix := ""
 	if ctx.Config().KatiEnabled() {
 		suffix = "-soong"
