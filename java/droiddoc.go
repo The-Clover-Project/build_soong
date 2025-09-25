3b0bcec0b8262101fc472ab6af3de71af8257695
Jim Tang <jimtang@google.com>
2025-09-25 05:57:48 -0700

    Revert "Add publish_dir property for droiddoc"
    Revert submission 35495652-sdk-docs
    
    Reason for revert: No need to extract zip to out/target/common/docs
    
    Reverted changes: /q/submissionid:35495652-sdk-docs
    
    Bug: 437232397
    Change-Id: Icb11eb1271e85f813e9a7e6a0fb59eae87ce7935

--- java/droiddoc.go
+++ java/droiddoc.go
@@ -16,7 +16,6 @@ package java
 
 import (
 	"fmt"
-	"path"
 	"path/filepath"
 	"strings"
 
@@ -166,9 +165,6 @@ type DroiddocProperties struct {
 
 	// Compat config XML. Generates compat change documentation if set.
 	Compat_config *string `android:"path"`
-
-	// The directory name to publish the generated documentation under out/target/common/docs.
-	Publish_dir *string
 }
 
 // Common flags passed down to build rule
@@ -865,12 +861,6 @@ func (d *Droiddoc) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		FlagWithOutput("-o ", d.docZip).
 		FlagWithArg("-C ", outDir.String()).
 		FlagWithArg("-D ", outDir.String())
-
-	if String(d.properties.Publish_dir) != "" {
-		publishDir := path.Join("out/target/common/docs", String(d.properties.Publish_dir))
-		rule.Command().Text("mkdir -p").Text(publishDir)
-		rule.Command().Text("unzip -qo").Input(d.docZip).Text("-d").Text(publishDir)
-	}
 
 	rule.Restat()
 
