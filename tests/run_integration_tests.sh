8dc475343725adca2f3067c3ee100450fd2a5552
Colin Cross <ccross@android.com>
2025-10-17 16:32:32 -0700

    Reuse mock top in lib.sh
    Move the customizations to the mock top for java_partial_compile_test.sh
    and androidmk_test.sh into extensions that reuse the main mock top, and
    reuse the main mock top between the various tests called by
    run_integration_tests.sh.
    
    For java_partial_compile_test.sh, move the initial build of impl-library
    with partial compile enabled into the warmed up mock top so the individual
    tests can just modify the tree and run partial compile again.  Also only
    run the full compile on the final state of the tree, there's no need to
    start from scratch and re-modify the tree for the full compile.
    
    Bug: 450580777
    Test: run_integration_tests.sh
    Change-Id: I0ba4bc99d936d0a4f485517d79fb8267b1e0f4ff

--- tests/run_integration_tests.sh
+++ tests/run_integration_tests.sh
@@ -3,6 +3,12 @@
 set -o pipefail
 
 TOP="$(readlink -f "$(dirname "$0")"/../../..)"
+
+# Pre-warm the mock top and export it to all the subtests
+source "$TOP/build/soong/tests/lib.sh"
+export WARMED_UP_MOCK_TOP
+export MOCK_TOP
+
 "$TOP/build/soong/tests/androidmk_test.sh"
 "$TOP/build/soong/tests/bootstrap_test.sh"
 "$TOP/build/soong/tests/soong_test.sh"
