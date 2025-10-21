4e2dfb5e37aae710d0474afc47a9e020f316f85a
Colin Cross <ccross@android.com>
2025-10-21 09:03:58 -0700

    Add cc_genrule_defaults
    cc_defaults doesn't work for cc_genrule modules because it doesn't
    have the genrule properties in it.  Adding the genrule properties
    to cc_defaults would add unnecessary properties that would be
    confusing in most contexts where cc_genrule is used.  Add a new
    cc_genrule_defaults that has the regular genrule properties plus
    the extra cc_genrule properties like vendor_available.
    
    Fixes: 453616468
    Test: TestGenruleDefaults
    Change-Id: I656c5e5d7af8d055a7699785913ce4039a789b41

--- cc/genrule_test.go
+++ cc/genrule_test.go
@@ -294,3 +294,31 @@ func TestGenruleToolWithSymlinks(t *testing.T) {
 		t.Errorf("Symlinks found")
 	}
 }
+
+func TestGenruleDefaults(t *testing.T) {
+	bp := `
+	cc_genrule_defaults {
+	    name: "defaults",
+		tool_files: ["tool"],
+		cmd: "$(location tool) $(in) $(out)",
+		out: ["out"],
+		vendor_available: true,
+		product_available: true,
+	}
+
+	cc_genrule {
+		name: "gen",
+		defaults: ["defaults"],
+	}
+	`
+	t.Helper()
+	ctx := PrepareForIntegrationTestWithCc.RunTestWithBp(t, bp)
+
+	variants := ctx.ModuleVariantsForTests("gen")
+	if !slices.Contains(variants, "android_vendor_arm64_armv8-a") {
+		t.Errorf(`expected vendor variant, but does not exist in %v`, variants)
+	}
+	if !slices.Contains(variants, "android_product_arm64_armv8-a") {
+		t.Errorf(`expected product variant, but does not exist in %v`, variants)
+	}
+}
