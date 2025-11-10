2d2d3d9e4cd297e8f87e25a0b1798a8823bb0acd
Matthew Maurer <mmaurer@google.com>
2025-11-10 16:59:50 +0000

    rust: Support strip overrides in rust_defaults
    Test: None
    Change-Id: I1bb44df5e0d596e0cf1baa5980affe9e64f453b0

--- rust/rust.go
+++ rust/rust.go
@@ -716,6 +716,7 @@ func DefaultsFactory(props ...any) android.Module {
 		&cc.RustBindgenClangProperties{},
 		&ClippyProperties{},
 		&SanitizeProperties{},
+		&cc.StripProperties{},
 		&fuzz.FuzzProperties{},
 	)
 
