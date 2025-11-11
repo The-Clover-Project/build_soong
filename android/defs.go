29479edb7a1e8eb189abfbd88c77a205b05f3552
Zi Wang <mrziwang@google.com>
2025-11-11 12:51:57 -0800

    Sandbox Android.Cp rule
    This change enables the sandboxing of Android.Cp rule, at the same
    time, it also disables the sandboxing of CpExecutableWithBashBootstrap
    rule.
    
    Test: SOONG_ACTION_SANDBOXING=nsjail m and TH
    Bug: 457763642
    Change-Id: I6dd3009dbea9366898e16fd204299580305f756c

--- android/defs.go
+++ android/defs.go
@@ -51,9 +51,9 @@ var (
 	// A copy rule.
 	Cp = pctx.AndroidStaticRule("Cp",
 		blueprint.RuleParams{
-			Command:         "rm -f $out && cp $cpPreserveSymlinks $cpFlags $in $out$extraCmds",
-			Description:     "cp $out",
-			SandboxDisabled: true,
+			Command:     "${rm} -f $out && ${cp} $cpPreserveSymlinks $cpFlags $in $out$extraCmds",
+			CommandDeps: []string{"${rm}", "${cp}"},
+			Description: "cp $out",
 		},
 		"cpFlags", "extraCmds")
 
@@ -113,9 +113,10 @@ var (
 	// A copy executable rule wrapped with bash with bootstrapping
 	CpExecutableWithBashBootstrap = pctx.AndroidStaticRule("CpExecutableWithBashBootstrap",
 		blueprint.RuleParams{
-			Command:     "/bin/bash -c \"(${rmSrc} -f $out && ${cpSrc} $cpFlags $cpPreserveSymlinks $in $out ) && (${chmodSrc} +x $out$extraCmds )\"",
-			CommandDeps: []string{"${rmSrc}", "${cpSrc}", "${chmodSrc}", "${toybox}"},
-			Description: "cp $out",
+			Command:         "/bin/bash -c \"(${rmSrc} -f $out && ${cpSrc} $cpFlags $cpPreserveSymlinks $in $out ) && (${chmodSrc} +x $out$extraCmds )\"",
+			CommandDeps:     []string{"${rmSrc}", "${cpSrc}", "${chmodSrc}", "${toybox}"},
+			Description:     "cp $out",
+			SandboxDisabled: true,
 		},
 		"cpFlags", "extraCmds")
 
