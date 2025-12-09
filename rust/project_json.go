07aad9a905395ee40ed931e1a7475d322fa8ccbe
Stefan Bossbaly <sbossb@gmail.com>
2025-12-09 21:18:25 +0000

    Land Connects clippy errors to a form rust analyzer can read
    Test: m
    bug: b/423950884
    Co-developed-by: Charisee Chiw and  Tamir Duberstein
    Change-Id: Id1008e595f1de99e66c9712e0f21682a7b1234ba

--- rust/project_json.go
+++ rust/project_json.go
@@ -17,10 +17,11 @@ package rust
 import (
 	"encoding/json"
 	"fmt"
-        "strings"
-        "github.com/google/blueprint/proptools"
+	"strings"
 
-        "android/soong/android"
+	"github.com/google/blueprint/proptools"
+
+	"android/soong/android"
 	"android/soong/rust/config"
 )
 
@@ -30,13 +31,14 @@ import (
 // called.  This singleton is enabled only if SOONG_GEN_RUST_PROJECT is set.
 // For example,
 //
-//   $ SOONG_GEN_RUST_PROJECT=1 m nothing
+//   $ SOONG_GEN_RUST_PROJECT=1 SOONG_LINK_RUST_PROJECT_TO=${ANDROID_BUILD_TOP} m nothing
 
 const (
 	// Environment variables used to control the behavior of this singleton.
 	envVariableCollectRustDeps = "SOONG_GEN_RUST_PROJECT"
 	envVariableUseKythe        = "XREF_CORPUS"
 	rustProjectJsonFileName    = "rust-project.json"
+	rustTargetMappingFileName  = "rust-target-mapping.json"
 )
 
 // The format of rust-project.json is not yet finalized. A current description is available at:
@@ -64,6 +66,17 @@ type rustProjectJson struct {
 	Crates  []rustProjectCrate `json:"crates"`
 }
 
+type rustTargetMappingsJson []rustTargetMappingJson
+
+type rustTargetMappingJson struct {
+	Name               string `json:"name"`
+	BuildTarget        string `json:"build_target"`
+	CheckTarget        string `json:"check_target"`
+	SourceDir          string `json:"source_dir"`
+	TargetProduct      string `json:"TARGET_PRODUCT"`
+	TargetBuildVariant string `json:"TARGET_BUILD_VARIANT"`
+}
+
 type rustProjectIncludeDirs struct {
 	Include_dirs []string `json:"include_dirs"`
 	Exclude_dirs []string `json:"exclude_dirs"`
@@ -77,8 +90,9 @@ type crateInfo struct {
 }
 
 type projectGeneratorSingleton struct {
-	project     rustProjectJson
-	knownCrates map[string]crateInfo // Keys are module names.
+	project        rustProjectJson
+	targetMappings rustTargetMappingsJson
+	knownCrates    map[string]crateInfo // Keys are module names.
 }
 
 func rustProjectGeneratorSingleton() android.Singleton {
@@ -191,6 +205,23 @@ func (singleton *projectGeneratorSingleton) addCrate(ctx android.SingletonContex
 		singleton.project.Crates = append(singleton.project.Crates, crate)
 	}
 	singleton.knownCrates[module.Name()] = crateInfo{Idx: idx, Deps: deps, Device: commonInfo.Target.Os.Class == android.Device}
+	if rustInfo.CompilerInfo.BuildTarget != nil {
+		buildVariant := "user"
+		if ctx.Config().Eng() {
+			buildVariant = "eng"
+		} else if ctx.Config().Debuggable() {
+			buildVariant = "userdebug"
+		}
+		mapping := rustTargetMappingJson{
+			Name:               module.Name(),
+			BuildTarget:        rustInfo.CompilerInfo.BuildTarget.String(),
+			CheckTarget:        rustInfo.CompilerInfo.CheckTarget.String(),
+			SourceDir:          ctx.ModuleDir(module),
+			TargetProduct:      ctx.Config().DeviceProduct(),
+			TargetBuildVariant: buildVariant,
+		}
+		singleton.targetMappings = append(singleton.targetMappings, mapping)
+	}
 	return idx, true
 }
 
@@ -233,6 +264,7 @@ func (singleton *projectGeneratorSingleton) GenerateBuildActions(ctx android.Sin
 	err := createJsonFile(singleton.project, path)
 	if err != nil {
 		ctx.Errorf(err.Error())
+
 	}
 	if ctx.Config().XrefCorpusName() != "" {
 		rule := android.NewRuleBuilder(pctx, ctx).SandboxDisabled()
@@ -249,15 +281,21 @@ func (singleton *projectGeneratorSingleton) GenerateBuildActions(ctx android.Sin
 		rule.Build(ruleName, "Turning Rust project into kzips")
 		ctx.Phony("xref_rust", kzipPath)
 	}
+
+	rustTargetMappingPath := android.PathForOutput(ctx, rustTargetMappingFileName)
+	if err := createJsonFile(singleton.targetMappings, rustTargetMappingPath); err != nil {
+		ctx.Errorf(err.Error())
+	}
+
 }
 
-func createJsonFile(project rustProjectJson, rustProjectPath android.WritablePath) error {
+func createJsonFile(project any, rustProjectPath android.WritablePath) error {
 	buf, err := json.MarshalIndent(project, "", "  ")
 	if err != nil {
-		return fmt.Errorf("JSON marshal of rustProjectJson failed: %s", err)
+		return fmt.Errorf("JSON marshal failed: %s", err)
 	}
-	err = android.WriteFileToOutputDir(rustProjectPath, buf, 0666)
-	if err != nil {
+
+	if err := android.WriteFileToOutputDir(rustProjectPath, buf, 0666); err != nil {
 		return fmt.Errorf("Writing rust-project to %s failed: %s", rustProjectPath.String(), err)
 	}
 	return nil
