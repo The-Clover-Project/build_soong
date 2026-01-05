761ba4565a0c0fcc6d2899984d37893500d0de02
Jared Duke <jdduke@google.com>
2026-01-05 23:04:41 +0000

    Make proguard_compatibility configurable with select()
    Allow configuration of the `optimize: proguard_compatibility` property
    via build flags or other soong variables.
    
    In practice, the default is false for all targets (as
    RELEASE_R8_FULL_MODE_BY_DEFAULT has fully ramped). However, there remain
    a handful of targets who are explicitly opted in, and we want to support
    flag-based relaxation for such targets.
    
    This change should be a noop given the state of module defaults (for app
    targets) and the global default (for RELEASE_R8_FULL_MODE_BY_DEFAULT).
    
    Bug: 215530220
    Test: presubmit
    Flag: build.RELEASE_R8_FULL_MODE_BY_DEFAULT
    Change-Id: I6c561774703a909a55d33172595398ae4d2509ce

--- java/app.go
+++ java/app.go
@@ -1487,7 +1487,6 @@ func AndroidAppFactory() android.Module {
 
 	module.Module.dexProperties.Optimize.EnabledByDefault = true
 	module.Module.dexProperties.Optimize.ShrinkByDefault = true
-	module.Module.dexProperties.Optimize.Proguard_compatibility = proptools.BoolPtr(false)
 
 	module.Module.properties.Instrument = true
 	module.Module.properties.Supports_static_instrumentation = true
@@ -1951,7 +1950,6 @@ func AndroidTestHelperAppFactory() android.Module {
 	// TODO(b/192032291): Disable by default after auditing downstream usage.
 	module.Module.dexProperties.Optimize.EnabledByDefault = true
 	module.Module.dexProperties.Optimize.Ignore_library_extends_program = proptools.BoolPtr(true)
-	module.Module.dexProperties.Optimize.Proguard_compatibility = proptools.BoolPtr(false)
 
 	module.Module.properties.Installable = proptools.BoolPtr(true)
 	module.appProperties.Use_embedded_native_libs = proptools.BoolPtr(true)
