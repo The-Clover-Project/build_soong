f9f8ab77190e27650b541b371dd2875c9847ec62
Roland Levillain <rpl@google.com>
2026-01-21 05:06:49 -0800

    C is (temporarily) 10000.
    It's not a fifty-four-year-old programming language; it's not an
    element common to all known life; it's (almost!) the next Android
    release!
    
    Bug: 435123598
    Bug: 477504044
    Test: Presubmits of a topic including this CL and ag/37461435
    Change-Id: I4d59cd79d39e156fc17a72e93b43be6efcbbf53c

--- android/api_levels.go
+++ android/api_levels.go
@@ -472,6 +472,7 @@ func getApiLevelsMapReleasedVersions() (map[string]int, error) {
 		"UpsideDownCake":  34,
 		"VanillaIceCream": 35,
 		"Baklava":         36,
+		"CinnamonBun":     10000,
 		"CANARY":          10000,
 	}, nil
 }
