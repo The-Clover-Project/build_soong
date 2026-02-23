1b1e40f0b8cce7bcff4a588b8c442d9c6cdf7517
Paul Duffin <paulduffin@google.com>
2026-02-23 23:32:50 +0000

    Pass --jdk-home to Metalava for droidstubs_host modules
    Previously, `droidstubs_host` modules did not supply any information
    about the JDK to Metalava which meant that it would not be able to
    resolve any information about `java.*` classes. This change rectifies
    that, passing in the `ANDROID_JAVA_HOME` directory along with the
    contents of that directory.
    
    That reduced the number of UnresolvedLink errors from 703 down to 21.
    
    Bug: 479907812
    Test: ./gradlew
    Change-Id: I9faff5f662b8b16388f1e969b797822e549c4379

--- java/droidstubs.go
+++ java/droidstubs.go
@@ -860,6 +860,14 @@ func metalavaCmd(ctx android.ModuleContext, rule *android.RuleBuilder, srcs andr
 		FlagWithRspFileInputList("@", android.PathForModuleOut(ctx, fmt.Sprintf("%s.metalava.rsp", params.stubsType.String())), srcs).
 		FlagWithInput("@", srcJarList)
 
+	// If this is for the host then pass the --jdk-home option to Metalava and
+	// make sure the files necessary to access the JDK class files are available.
+	if ctx.Host() {
+		homeDir := ctx.Config().Getenv("ANDROID_JAVA_HOME")
+		cmd.FlagWithArg("--jdk-home ", homeDir)
+		cmd.Implicits(ctx.GlobFilesOutsideModuleDir(filepath.Join(homeDir, "**/*"), nil))
+	}
+
 	// Metalava does not differentiate between bootclasspath and classpath and has not done so for
 	// years, so it is unlikely to change any time soon.
 	combinedPaths := append(([]android.Path)(nil), params.deps.bootClasspath.Paths()...)
